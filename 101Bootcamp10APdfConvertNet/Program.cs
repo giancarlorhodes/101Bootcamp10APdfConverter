8aeb20d461ed968aa2728ae1e93cd7237f4d31fd
giancarlorhodes <giancarlo.rhodes@gmail.com>
2022-04-15 13:34:14 -0500

    Add project files.

---
+++ 101Bootcamp10APdfConvertNet/Program.cs
@@ -0,0 +1,52 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _101Bootcamp10APdfConvertNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Create a new PDF document
+            PdfDocument document = new PdfDocument();
+            document.Info.Title = "Created with PDFsharp";
+
+            // Create an empty page
+            PdfPage page = document.AddPage();
+
+            // Get an XGraphics object for drawing
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            // Create a font
+            PdfSharp.Drawing.XFont font = new PdfSharp.Drawing.XFont("Verdana", 20, XFontStyle.BoldItalic);
+
+            // Draw the text
+            gfx.DrawString("Hello, World! I am using PdfSharp !!!", font, XBrushes.Black,
+            new XRect(0, 0, page.Width, page.Height),
+            XStringFormats.Center);
+
+            // TODO - how to do line breaks ????
+            //gfx.DrawString("http://www.pdfsharp.net/wiki/HelloWorld-sample.ashx\n", font, XBrushes.Black,
+            //new XRect(0, 0, page.Width, page.Height),
+            //XStringFormats.Center);
+            //gfx.DrawString(".NET 4.8, core 3.1 thru an error\n", font, XBrushes.Black,
+            //new XRect(0, 0, page.Width, page.Height),
+            //XStringFormats.Center);
+
+            // Save the document...
+            const string filename = "HelloWorld.pdf";
+            document.Save(filename);
+
+            // ...and start a viewer.
+            Process.Start(filename);
+
+        }
+    }
+}
---
+++ 101Bootcamp10APdfConverter/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace _101Bootcamp10APdfConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Create a new PDF document
+            PdfDocument document = new PdfDocument();
+            document.Info.Title = "Created with PDFsharp";
+
+            // Create an empty page
+            PdfPage page = document.AddPage();
+
+            // Get an XGraphics object for drawing
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+
+            // Create a font
+            PdfSharp.Drawing.XFont font = new PdfSharp.Drawing.XFont("Verdana", 20, XFontStyle.BoldItalic);
+           
+
+            // Draw the text
+            gfx.DrawString("Hello, World!", font, XBrushes.Black,
+            new XRect(0, 0, page.Width, page.Height),
+            XStringFormats.Center);
+
+
+
+            // Save the document...
+            const string filename = "HelloWorld.pdf";
+            document.Save(filename);
+
+            // ...and start a viewer.
+            Process.Start(filename);
+
+        }
+    }
+}
